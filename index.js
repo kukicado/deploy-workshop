4df12e24e01687a7fa9a68fed7b9d9d38802f399
Ado Kukic <kukicadnan@gmail.com>
2022-11-16 10:56:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 // Bring in our dependencies
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const MongoClient = require("mongodb").MongoClient;
 const { Client } = require("pg");
 
@@ -34,9 +35,51 @@ app.get("/mdb-customers", async (req, res) => {
   try {
     const data = await MDBClient.db("deploy-store")
       .collection("customers")
-      .find()
-      .limit(10)
+      .findOne({ _id: new ObjectId("63752e251ecd2d5859ab36dc") });
+    res.json(data);
+  } catch (e) {
+    res.json(e);
+  }
+});
+
+app.get("/mdb-add-customer", async (req, res) => {
+  try {
+    const result = await MDBClient.db("deploy-store")
+      .collection("customers")
+      .insertOne({ fullName: "Akhil", job: "Being awesome moderator" });
+    res.json(result);
+  } catch (e) {
+    res.json(e);
+  }
+});
+
+app.get("/mdb-aggregation", async (req, res) => {
+  try {
+    const data = await MDBClient.db("deploy-store")
+      .collection("orders")
+      .aggregate([
+        {
+          $group: {
+            _id: "$customer.fullName",
+            totalOrders: {
+              $count: {},
+            },
+            totalItemsPurchased: {
+              $sum: {
+                $size: "$items",
+              },
+            },
+            totalSpend: {
+              $sum: "$total",
+            },
+          },
+        },
+        {
+          $limit: 10,
+        },
+      ])
       .toArray();
+
     res.json(data);
   } catch (e) {
     res.json(e);
@@ -45,8 +88,21 @@ app.get("/mdb-customers", async (req, res) => {
 
 app.get("/pg-customers", async (req, res) => {
   try {
-    const data = await PGClient.query("SELECT * FROM customers LIMIT 10");
+    const data = await PGClient.query(
+      "SELECT * FROM customers WHERE full_name = 'Aaishika'"
+    );
     res.json(data.rows);
+  } catch (e) {
+    res.json(e);
+  }
+});
+
+app.get("/pg-add-customer", async (req, res) => {
+  try {
+    const result = await PGClient.query(
+      "INSERT INTO customers (id, full_name) VALUES ('12345', 'Aaishika')"
+    );
+    res.json(result);
   } catch (e) {
     res.json(e);
   }
